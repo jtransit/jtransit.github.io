5508323f11d3d35c48a40064487e7e60b24f783a
Ivan <ivanrodgearpos@gmail.com>
2019-09-03 23:47:15 +0800

    Major Update

--- js/jtransit.js
+++ js/jtransit.js
@@ -7,17 +7,37 @@ $(document).ready(function(){
         if(scroll_pos > (masthead + window)) {
             $("#features").css('background-color', '#17263c');
             $("#features").css('color', 'white');
-            $("#features").css('transition', 'background-color 0.75s');
+            $("#features").css('transition', 'background-color 0.5s');
             $('#feature-title').text('Introducing Night Mode');
-            $('#pic-2').addClass('hidden');
-            $('#pic-1').removeClass('hidden');
+            // $('#pic-2').addClass('hidden');
+            $( '#pic-1' ).attr("src","img/night-mode.jpg");
+            // $('#pic-1').removeClass('hidden');
         } else {
             $("#features").css('background-color', 'white');
             $("#features").css('color', 'black');
             $("#features").css('transition', 'background-color 0.3s');
             $('#feature-title').text('New Features are Available');
-            $('#pic-1').addClass('hidden');
-            $('#pic-2').removeClass('hidden');
+            $( '#pic-1' ).attr("src","img/light-mode.jpg");
         }
+    });
+
+    $("#item-1").hover(function() {
+        $( '#pic-1' ).attr("src","img/light-mode.jpg");
+        }, function() {
+    });
+
+    $("#item-2").hover(function() {
+        $( '#pic-1' ).attr("src","img/route-suggestion.jpg");
+        }, function() {
+    });
+    
+    $("#item-3").hover(function() {
+        $( '#pic-1' ).attr("src","img/fare-calculation.jpg");
+        }, function() {
+    });
+
+    $("#item-4").hover(function() {
+        $( '#pic-1' ).attr("src","img/offline-routes.jpg");
+        }, function() {
     });
 });
