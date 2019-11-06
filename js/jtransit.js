c5936092c1ef76e7c1bfe5e9522352dde88329e3
Webster Dave Bontilao <wbontilao@csyoutsourcing.com>
2019-11-06 15:50:10 +0800

    Updated night mode for main section of web page

--- js/jtransit.js
+++ js/jtransit.js
@@ -74,6 +74,8 @@ $(document).ready(function(){
         $("#features").css('transition', 'background-color 0.5s');
         $('#feature-title').text('Introducing Night Mode');
         $( '#pic-1' ).attr("src","img/night-mode.jpg");
+        $('header.masthead').css('background', '#17263c');
+        $('#mainNav .navbar-brand').css('color', '#fff !important');
     }
 
     function light_mode() {
@@ -84,5 +86,7 @@ $(document).ready(function(){
         $("#features").css('transition', 'background-color 0.3s');
         $('#feature-title').text('New Features are Available');
         $( '#pic-1' ).attr("src","img/light-mode.jpg");
+        $('header.masthead').css('background', '#ff7f57');
+        $('#mainNav .navbar-brand').css('color', '#17263c !important');
     }
 });
